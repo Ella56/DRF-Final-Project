7435ec11f8fe38e4d41eacc7674b28960f480b68
YVismeh <yvisme41148@gmail.com>
2025-03-29 17:58:50 +0330

    service api

--- service/api/v1/serializer.py
+++ service/api/v1/serializer.py
@@ -1,1 +1,21 @@
 from rest_framework import serializers
+from service.models import Service, Special_services
+from portfolio.models import Category
+from accounts.models import User
+
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Service
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        cat = []
+        for category_id in rep["category"]:
+            title = Category.objects.get(id=category_id).title
+            cat.append(title)
+        rep["category"] = cat
+        return rep
