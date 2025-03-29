9c82d0d8003d7dc8d89eecfc15423425a0606982
YVismeh <yvisme41148@gmail.com>
2025-03-29 19:18:26 +0330

    about - MVT

--- root/urls.py
+++ root/urls.py
@@ -5,4 +5,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
+    path('about',AboutView.as_view(),name='about'),
+
 ]
--- root/views.py
+++ root/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
 from .forms import ContactForm
+from .models import Team, Client
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -8,7 +9,14 @@ class HomeView(TemplateView):
 
 
 class AboutView(TemplateView):
-    pass
+    template_name = 'root/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["teams"] = Team.objects.filter(status=True)[:4]
+        context["clients"] = Client.objects.filter(status=True)
+
+        return context
 
 
 class ContactView(FormView):
