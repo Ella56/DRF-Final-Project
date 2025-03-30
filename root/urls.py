80f39a42cfd8d6a3986b143aaaa3a3f9aa3ea877
Erfan Shafiei <157620263+ErfanShafiei47@users.noreply.github.com>
2025-03-30 03:49:26 +0330

    views and urls (v1)
    views added
    urls added
    jinjas added

--- root/urls.py
+++ root/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import HomeView, ContactView, AboutView, TestimonialView
+from .views import HomeView, contact, AboutView, TestimonialView
 
 app_name = 'home'
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
+    path('about', AboutView.as_view(), name='about'),
+    path('contact', contact , name='contact'),
+    path('testimonial', TestimonialView.as_view(), name='testimonial'),
 ]
