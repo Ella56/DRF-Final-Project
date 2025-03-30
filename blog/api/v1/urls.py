1a2e8e515b069dfa57b3ce535119211f4bc8284e
xcouter05 <xcouter302@gmail.com>
2025-03-30 18:58:48 +0330

    api-blog

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import *
-
-
+from .views import BlogViewSet, CommentViewSet
 
 urlpatterns = [
-    path(),
-    
+    path('',BlogViewSet.as_view({"get": "list", "post": "create"}), name="blog-list"),
+    path("<int:pk>/",BlogViewSet.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}), name="blog-detail"),
+
+    path("comments/", CommentViewSet.as_view({"get": "list", "post": "create"}), name="comment-list"),
+    path("comments/<int:pk>/", CommentViewSet.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}), name="comment-detail"),
 ]
