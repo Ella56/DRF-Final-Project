267dc2bea6bdddc462a755dd80ea8741bfc5a714
Ella56 <zamani56.elham@gmail.com>
2025-04-03 12:54:41 +0330

    blog-blog detail

--- blog/views.py
+++ blog/views.py
@@ -12,25 +12,26 @@ class BlogView(ListView):
     model = Blog
     template_name = 'blog/blog.html'
     context_object_name = "blogs"
-    paginate_by = 1
+    paginate_by = 8
     
     def get_queryset(self):
         if self.kwargs.get("category"):
-            blogs = self.model.objects.filter(category__title=self.kwargs.get("category"))
+            blogs = self.model.objects.filter(category__name=self.kwargs.get("category"))
         elif self.kwargs.get("tag"):
-            blogs = self.model.objects.filter(tag__title=self.kwargs.get("tag"))
+            blogs = self.model.objects.filter(tag__name=self.kwargs.get("tag"))
             # elif self.kwargs.get("name"):
             #blogs = Blog.objects.filter(creator__user__email=self.kwargs.get("name"), status=True)
         elif self.request.GET.get('search') is not None:
             search = self.request.GET.get('search')
-            blogs = Blog.objects.filter(desc1__contains = search)
+            blogs = Blog.objects.filter(title__contains = search)
         else:
             blogs = Blog.objects.filter(status=True).order_by('-created_at')
+
         return blogs
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['category'] = Blog_category.objects.filter(status=True)
+        context['category'] = Blog_category.objects.all()
         context['tag'] = Tag.objects.filter(status=True)
         first = 1
         blogs_paginate = Paginator(self.get_queryset(), 2)
@@ -52,7 +53,8 @@ class BlogDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm
-        context['comments'] = Comment.objects.filter(status=True, blog=Blog.id)
+        blog = get_object_or_404(Blog, id=self.kwargs['pk'])
+        context['comments'] = Comment.objects.filter(status=True, blog=blog.id)
         context["blogs"] = Blog.objects.filter(status=True).order_by("-created_at")[:5]
         return context
     
@@ -68,9 +70,10 @@ class BlogDetailView(DetailView):
                 comment.name = request.user
                 parent_id = request.POST.get("parent_id") #check if its reply
                 if parent_id:
+                    parent_id = int(parent_id)
                     parent_comment = get_object_or_404(Comment, id= parent_id)
                     comment.parent = parent_comment # set parent if its reply
-                comment.save()
+                    comment.save()
                 messages.add_message(self.request, messages.SUCCESS, "Your comments has been successfully sent ")
                 return redirect(self.request.path_info, pk=blog.pk)
             else:
--- context_processor.py
+++ context_processor.py
@@ -4,6 +4,7 @@ from portfolio.models import Category
 
 def general_context(request):
     context = {
-        "cart_items" : len(request.session.get("cart" , {})) 
+        "cart_items" : len(request.session.get("cart" , {})) ,
+        'categories' : Category.objects.all(),
     }
     return context
